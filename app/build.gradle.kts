17803b77cf58a105e02d01c99f92030e51453370
NDescoteaux3 <104236869+NDescoteaux3@users.noreply.github.com>
2025-02-22 15:07:26 +0100

    start of step 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.navigation.ui.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/dadm/ndescot/quotationshake/ui/MainActivity.kt
+++ app/src/main/java/dadm/ndescot/quotationshake/ui/MainActivity.kt
@@ -1,19 +1,28 @@
 package dadm.ndescot.quotationshake.ui
 
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import dadm.ndescot.quotationshake.R
 import dadm.ndescot.quotationshake.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +31,42 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.bottomNavigationView) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.displayCutout() or
+                    WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                left = bars.left,
+                top = 0,
+                right = 0,
+                bottom = bars.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.navHostFragment) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.displayCutout() or
+                    WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                left = 0,
+                top = 0,
+                right = bars.right,
+                bottom = if (resources.configuration.orientation == ORIENTATION_LANDSCAPE) bars.bottom else 0
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.appBarLayout) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.displayCutout() or
+                    WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                left = bars.left,
+                top = 0, //Todo instructions say to put bars.top here but it looks bad
+                right = 0,
+                bottom = 0
+            )
+            WindowInsetsCompat.CONSUMED
+        }
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -38,10 +83,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val navController =
+        navController =
             binding.navHostFragment.getFragment<NavHostFragment>().navController
-        binding.bottomNavigationView.setupWithNavController(navController)
+        (binding.bottomNavigationView as NavigationBarView).setupWithNavController(navController)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        addMenuProvider(this)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.menu_about -> {
+                navController.navigate(R.id.aboutDialogFragment)
+                true
+            }
+            else -> false
+        }
     }
 }
